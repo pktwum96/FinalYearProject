cde1c1ea131cfddff73da4a45afc3b4d6579b7d2
Kofi Twum-Ampofo <pktwum96@gmail.com>
2019-02-21 15:43:13 +0000

    Progress with dashboard, navbar sidebar dark theme

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,105 +7,91 @@ import '../styles/Dashboard.css';
 
 class Dashboard extends Component {
 
-    constructor (){
-      super();
-      this.state = {
+  constructor (){
+    super();
+    this.state = {
 
-        active:false
-      }
-      this.changeClass = e => {
-        e.preventDefault();
-        this.setState({active: !this.state.active})
-      }
+      active:false
     }
+    this.changeClass = e => {
+      e.preventDefault();
+      this.setState({active: !this.state.active})
+    }
+  }
 
 
 
   render() {
     return (
       <div>
-        <div className={this.state.active ? "d-flex toggled" : "d-flex"} id="wrapper" >
+        <div className={this.state.active ? "d-flex toggled active" : "d-flex"} id="wrapper" >
 
           {/* Sidebar */}
-          <div className="bg-light border-right" id="sidebar-wrapper">
-            <div className="sidebar-heading">Start Bootstrap </div>
+          <div className="bg-dark border-right" id="sidebar-wrapper">
+            <div className="sidebar-heading">iPortfolio</div>
             <div className="list-group list-group-flush">
-              <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Events</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Profile</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Status</a>
+              <a href="#" className="list-group-item list-group-item-action bg-dark">
+                <i class="material-icons" >poll</i>
+                <h5>Dashboard</h5>
+              </a>
+              <a href="#" className="list-group-item list-group-item-action bg-dark">
+                <i class="material-icons" >work</i>
+                <h5>Assessment</h5>
+              </a>
+              <a href="#" className="list-group-item list-group-item-action bg-dark">
+                <i class="material-icons">account_balance_wallet</i>
+                <h5>Invest</h5>
+              </a>
+              <a href="#" className="list-group-item list-group-item-action bg-dark">
+                <i class="material-icons" >school</i>
+                <h5>Learn</h5>
+              </a>
             </div>
           </div>
           {/* /#sidebar-wrapper */}
 
           {/* Page Content */}
           <div id="page-content-wrapper">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <a className="sidebartoggler" onClick={this.changeClass}><i className="material-icons">apps</i></a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav ml-auto">
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-
-              <div id="content">
-                <button type="button" id="sidebarCollapse" className="navbar-btn" onClick={this.changeClass.bind(this)}>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </button>
+                  <li className="nav-item">
+                    <a><i className="material-icons">announcement</i></a>
+                  </li>
+                  <li className="nav-item">
+                    <a><i className="material-icons">notifications_none</i></a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i className="material-icons">account_circle</i>
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a className="dropdown-item" href="#">Portfolio</a>
+                      <a className="dropdown-item" href="#">Wallet</a>
+                      <a className="dropdown-item" href="#">Logout</a>
+                    </div>
+                  </li>
+                </ul>
               </div>
+            </nav>
 
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                      <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                      </li>
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Dropdown
-                        </a>
-                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                          <a className="dropdown-item" href="#">Action</a>
-                          <a className="dropdown-item" href="#">Another action</a>
-                          <div className="dropdown-divider"></div>
-                          <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                </nav>
-
-                <div className="container-fluid">
-                  <h1 className="mt-4">Simple Sidebar</h1>
-                  <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-                  <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
-                </div>
-              </div>
-              {/* /#page-content-wrapper */}
-
+            <div className="container-fluid">
             </div>
-            {/* /#wrapper */}
+          </div>
+          {/* /#page-content-wrapper */}
 
-            {/* Bootstrap core JavaScript */}
-            <script src="vendor/jquery/jquery.min.js"></script>
-            <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        </div>
+        {/* /#wrapper */}
 
-            {/* Menu Toggle Script
-              <script>
-              $("#menu-toggle").click(function(e) {
-              e.preventDefault();
-              $("#wrapper").toggleClass("toggled");
-              });
-              </script> */}
 
-            </div>
-          );
-        }
-      }
+      </div>
+    );
+  }
+}
 
-      export default Dashboard;
+export default Dashboard;
