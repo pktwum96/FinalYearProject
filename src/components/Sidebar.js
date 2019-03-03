c5c68674ebb4c80e6a9eb57d99c2ed703d4ae966
Kofi Twum-Ampofo <pktwum96@gmail.com>
2019-03-03 19:36:55 +0000

    Changed font awesome classname

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,35 +7,35 @@ class Sidebar extends React.Component {
         return (
             <div className="bg-dark border-right" id="sidebar-wrapper">
                 <div id="sticky">
-                    <div className="sidebar-heading"><i class="fas fa-info-circle"></i></div>
+                    <div className="sidebar-heading"><i className="fas fa-info-circle"></i></div>
 
                     <div className="list-group list-group-flush">
                         <ul className="nav nav-tabs">
 
                             <li className="active">
                                 <a href="/dashboard" className="list-group-item list-group-item-action bg-dark">
-                                    <i class="fas fa-chart-line"></i>
+                                    <i className="fas fa-chart-line"></i>
                                     <h5>Dashboard</h5>
                                 </a>
                             </li>
 
                             <li>
                                 <a href="/assessment" className="list-group-item list-group-item-action bg-dark">
-                                    <i class="fas fa-info-circle"></i>
+                                    <i className="fas fa-info-circle"></i>
                                     <h5>Assessment</h5>
                                 </a>
                             </li>
 
                             <li>
                                 <a href="/invest" className="list-group-item list-group-item-action bg-dark">
-                                    <i class="far fa-money-bill-alt"></i>
+                                    <i className="far fa-money-bill-alt"></i>
                                     <h5>Invest</h5>
                                 </a>
                             </li>
 
                             <li>
                                 <a href="/learn" className="list-group-item list-group-item-action bg-dark">
-                                    <i class="fas fa-graduation-cap"></i>
+                                    <i className="fas fa-graduation-cap"></i>
                                     <h5>Learn</h5>
                                 </a>
                             </li>
