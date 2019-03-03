c2d7ecc9249de3f06cf3b89699820e942eb34bc0
Kofi Twum-Ampofo <pktwum96@gmail.com>
2019-03-03 19:20:25 +0000

    Default Router

---
+++ src/components/Default.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Default extends React.Component{
+    render () {
+        return (
+            <div>
+                <h6>Page not found</h6>
+            </div>
+        )
+    }
+}
+
+export default Default;
