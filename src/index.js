1f31b9b60d9ce014e2e5fe8e5dc08bca2d18bf09
Kofi Twum-Ampofo <pktwum96@gmail.com>
2019-03-20 21:30:33 +0000

    minor edit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+var config = {
+  apiKey: "AIzaSyBHrDFb3ACBCwjsV5s1EVfKoWTbY22LAgY",
+  authDomain: "iportfolio-77124.firebaseapp.com",
+  databaseURL: "https://iportfolio-77124.firebaseio.com",
+  projectId: "iportfolio-77124",
+  storageBucket: "iportfolio-77124.appspot.com",
+  messagingSenderId: "600607936739"
+};
+
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
