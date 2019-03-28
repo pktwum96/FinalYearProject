f7b511f21110cf8302a88220a8f74ab1f740cb2b
Kofi Twum-Ampofo <pktwum96@gmail.com>
2019-03-28 18:25:32 +0000

    Removed config

--- src/index.js
+++ src/index.js
@@ -9,16 +9,6 @@ import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 
 
-var config = {
-  apiKey: "AIzaSyBHrDFb3ACBCwjsV5s1EVfKoWTbY22LAgY",
-  authDomain: "iportfolio-77124.firebaseapp.com",
-  databaseURL: "https://iportfolio-77124.firebaseio.com",
-  projectId: "iportfolio-77124",
-  storageBucket: "iportfolio-77124.appspot.com",
-  messagingSenderId: "600607936739"
-};
-
-
 firebase.initializeApp(config);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
